11e6f8425ebabaff1357c1cfd7df7be60bef4137
drehnstrom <drehnstrom@yahoo.com>
2025-09-29 14:54:58 -0400

    Minor Code Edits

--- challlenge-medical-diet-navigator/nutritian-flask-app/README.md
+++ challlenge-medical-diet-navigator/nutritian-flask-app/README.md
@@ -9,7 +9,8 @@ source venv/bin/activate
 pip install -r requirements.txt
 ```
 
-## To run locally, you will need a service account key 
+## To run locally, you will need a service account key. 
+Create this in your project and download it. Name it whatever you like and update the following code accordingly.
 ```
 export GOOGLE_APPLICATION_CREDENTIALS=~/agents-for-impact-key.json
 ```
--- challlenge-medical-diet-navigator/nutritian-flask-app/config.py
+++ challlenge-medical-diet-navigator/nutritian-flask-app/config.py
@@ -4,9 +4,13 @@ import os
 class Config:
     PROJECT_ID = os.getenv("PROJECT_ID", "cool-benefit-472616-t9")
     LOCATION = os.getenv("LOCATION", "us-central1")
-    SERVICE_ACCOUNT_EMAIL = os.getenv("SERVICE_ACCOUNT_EMAIL", "278550033422-compute@developer.gserviceaccount.com")
+
+    # Need to create a GCS bucket that images will be uploaded to when users submit them via the web app    
     UPLOAD_BUCKET = os.getenv("UPLOAD_BUCKET", "diet-navigator-uploads-cool-benefit-472616-t9") 
     
+    # This will need to be updated with your actual agent engine full address
     Agent_Engine_Full_Adress = os.getenv("Agent_Engine_Full_Adress", "projects/cool-benefit-472616-t9/locations/us-central1/reasoningEngines/6627156802838462464")
+    
+    
     # Add other Flask config settings if needed
     DEBUG = True
